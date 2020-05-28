473f5b7a595c9f9c2c0d017006967a5668896b28
MIN_GEUN <min0903@adcapsule.co.kr>
2020-05-28 13:31:36 +0900

    Updates

---
+++ precache-manifest.4ae71ba88339973a1c9486c74bc722d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbaffaa918ce73c66a454b89d5d21fbf",
+    "url": "/nomad-react-movie-app-2019.v2/index.html"
+  },
+  {
+    "revision": "ebf271e6b19e6f4dc08f",
+    "url": "/nomad-react-movie-app-2019.v2/static/css/main.b9ed9fa6.chunk.css"
+  },
+  {
+    "revision": "ec1cf157968acecc656e",
+    "url": "/nomad-react-movie-app-2019.v2/static/js/2.0ec84924.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad-react-movie-app-2019.v2/static/js/2.0ec84924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf271e6b19e6f4dc08f",
+    "url": "/nomad-react-movie-app-2019.v2/static/js/main.79bd12bb.chunk.js"
+  },
+  {
+    "revision": "2d07cd60da827589f3d9",
+    "url": "/nomad-react-movie-app-2019.v2/static/js/runtime-main.a8d2b0d7.js"
+  }
+]);
